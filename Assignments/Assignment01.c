c9a2a5a1ba8e6a69254bc72508edbc58575b31c8
Jack Mahedy <jackmahedy570@gmail.com>
2022-10-27 18:48:33 -0400

    Lab 1 Cleanup

--- Assignments/Assignment01.c
+++ Assignments/Assignment01.c
@@ -7,16 +7,16 @@ Program Description : This project displays my name, email,
 and assignment. Under this information is "CIT" made up of
 many letters.
 =========================================================*/
-define _CRT_SECURE_NO_WARNINGS							// to ignore different warnings.
-include <stdio.h>
-include <conio.h>
+#define _CRT_SECURE_NO_WARNINGS							// to ignore different warnings.
+#include <stdio.h>
+#include <curses.h>
 int main()
 {
 	printf("*************************\n");
 	printf("* Jack Mahedy		*\n");
 	printf("* jmahedy@purdue.edu	*\n");
 	printf("* CNIT105 Assignment-01	*\n");
-	printf("*************************\n\n\n\n\n\n");
+	printf("*************************\n\n\n");
 	printf("   cccc		IIIII		 ===============\n");
 	printf("  ccc		  I			T\n");
 	printf(" ccc		  I			T\n");
@@ -26,6 +26,5 @@ int main()
 	printf("   ccc		  I			T\n");
 	printf("     cccc	IIIII			T\n");
 
-	_getch();											// to keep the output screen open
 	return 0;
 }
